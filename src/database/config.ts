11e6c069c221529f24e6d22f287de31753cb721f
tetorgal <hector.galarza.css@gmail.com>
2024-10-20 07:46:05 -0600

    merge con 1-crear-server

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,15 +1,15 @@
-import { createClient } from "@supabase/supabase-js";
-import 'dotenv/config';
+import { createClient } from '@supabase/supabase-js'
+import 'dotenv/config'
 
-const supabaseUrl = process.env.supabaseURL as string;
-const supabaseKey = process.env.supabaseAPI_KEY as string;
+const supabaseUrl = process.env.supabaseURL as string
+const supabaseKey = process.env.supabaseAPI_KEY as string
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 if (!supabase) {
-    console.log("Error al conectar a Supabase");
+  console.log('Error al conectar a Supabase')
 } else {
-    console.log("Conectado a Supabase");
+  console.log('Conectado a Supabase')
 }
 
-export default supabase;
+export default supabase
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import 'dotenv/config';
-import supabase from './database/config';
+import 'dotenv/config'
+import supabase from './database/config'
 
-import cors from 'cors';
-import http from 'http';
-import express from 'express';
-import morgan from 'morgan';
+import cors from 'cors'
+import http from 'http'
+import express from 'express'
+import morgan from 'morgan'
 
 // Verificar la conexión con Supabase
 const testConnection = async () => {
@@ -13,37 +13,37 @@ const testConnection = async () => {
       .from('notes')
       .select('*')
       .order('id', { ascending: false })
-      .limit(10);
-      
+      .limit(10)
+
     if (error) {
-      console.log('Error trayendo datos', error);
+      console.log('Error trayendo datos', error)
     } else {
-      console.log('Datos trayendo', data);
+      console.log('Datos trayendo', data)
     }
   } catch (error) {
-    console.log('Error general de conexión', error);
+    console.log('Error general de conexión', error)
   }
-};
-testConnection();
+}
+testConnection()
 
 // Inicializar la aplicación de Express
-const app = express();
+const app = express()
 
 // Configurar middlewares
-app.use(cors());
-app.use(express.json());
-app.use(morgan('dev'));
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
 
 // Ruta básica
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+  res.send('Hello World!')
+})
 
 // Configuración del puerto
-const PORT = process.env.PORT || 5000;  // Usar puerto de la variable de entorno o 3000 por defecto
+const PORT = process.env.PORT || 5000 // Usar puerto de la variable de entorno o 3000 por defecto
 
 // Levantar el servidor
-const server = http.createServer(app);
+const server = http.createServer(app)
 server.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
-});
+  console.log(`Servidor escuchando en el puerto ${PORT}`)
+})
